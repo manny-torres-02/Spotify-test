823c28319db8fbf5937971700f20c9daf584c500
Manuel Torres <manueljosetorres02@gmail.com>
2024-08-20 23:51:26 -0500

    start working on auth flow

--- authorization_flow.js
+++ authorization_flow.js
@@ -1,1 +1,10 @@
+require.("dotenv").config();  // Load environment variables from .env file
 const clientID = process.env.SPOTIFY_CLIENT_ID;
+
+const code = undefined;  // The authorization code from the user
+
+if(!code) {
+  redirectToAuthCodeFlow(clientID);
+} else { 
+  const 
+}
