afc4641256c11751b3beac9f7e98fe1af9296ac0
Manuel Torres <manueljosetorres02@gmail.com>
2024-08-21 23:09:14 -0500

    push

--- authorization_flow.js
+++ authorization_flow.js
@@ -1,7 +1,7 @@
 require.("dotenv").config();  // Load environment variables from .env file
 const clientID = process.env.SPOTIFY_CLIENT_ID;
 const params = new URLSearchParams(window.location.search);
-const code = params.get(code);  // The authorization code from the user
+const code = params.get("code");  // The authorization code from the user
 
 if(!code) {
   redirectToAuthCodeFlow(clientID);
@@ -47,7 +47,24 @@ async function generateCodeChallenge(codeVerifier: string) {
       .replace(/=+$/, '');
 }
 
-async function getAccessToken(clientID, code) {
+export async function getAccessToken(clientId: string, code: string): Promise<string> {
+  const verifier = localStorage.getItem("verifier");
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("code_verifier", verifier!);
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: params
+  });
+
+  const { access_token } = await result.json();
+  return access_token;
 }
 
 async function fetchProfile(token) {
